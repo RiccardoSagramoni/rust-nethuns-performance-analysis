706509cccb734283d8ffe66ddde3a1d7977b1608
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-11-01 16:50:10 +0100

    Print stats about collected data

--- performance-analysis/src/main.rs
+++ performance-analysis/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::{env, mem};
 
 use plotly::color::NamedColor;
 use plotly::common::{DashType, Title};
@@ -7,21 +8,61 @@ use plotly::layout::{Axis, Shape, ShapeLine, ShapeType};
 use plotly::{BoxPlot, ImageFormat, Layout, Plot};
 
 
-const RUST_DATA_FILENAME: &str = "files/rust_data.txt";
-const CPP_DATA_FILENAME: &str = "files/cpp_data.txt";
-const OUTPUT_FILENAME: &str = "files/out.png";
-
 const REFERENCE_VALUE_Y: f64 = 12.0;
+
+#[derive(Debug)]
+struct Config {
+    rust_data_filename: String,
+    cpp_data_filename: String,
+    output_filename: String,
+}
+
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Stats {
+    mean: f64,
+    median: f64,
+    population_standard_deviation: f64,
+    population_variance: f64,
+    standard_deviation: f64,
+    variance: f64,
+}
 
 
 fn main() {
-    generate_box_plot(
-        parse_collected_data(RUST_DATA_FILENAME)
-            .expect("Failed to parse Rust data"),
-        parse_collected_data(CPP_DATA_FILENAME)
-            .expect("Failed to parse C++ data"),
-        OUTPUT_FILENAME,
+    let config = get_config();
+    println!(
+        "Start generating plots with the following configuration:\n{:#?}\n",
+        config
     );
+    
+    // Parse data
+    let rust_data = parse_collected_data(&config.rust_data_filename)
+        .expect("Failed to parse Rust data");
+    let cpp_data = parse_collected_data(&config.cpp_data_filename)
+        .expect("Failed to parse C++ data");
+    
+    // Print stats
+    println!("Rust stats:\n{:#?}\n", compute_stats(&rust_data));
+    println!("C++ stats:\n{:#?}\n", compute_stats(&cpp_data));
+    
+    // Generate plot
+    generate_box_plot(rust_data, cpp_data, &config.output_filename);
+}
+
+
+fn get_config() -> Config {
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        panic!("Usage: {} <rust_data_filename> <cpp_data_filename> <output_filename>", args[0]);
+    }
+    
+    Config {
+        rust_data_filename: mem::take(&mut args[1]),
+        cpp_data_filename: mem::take(&mut args[2]),
+        output_filename: mem::take(&mut args[3]),
+    }
 }
 
 
@@ -38,6 +79,20 @@ fn parse_collected_data(filename: &str) -> Result<Vec<f64>, io::Error> {
     Ok(vec)
 }
 
+
+fn compute_stats(data: &[f64]) -> Stats {
+    Stats {
+        mean: statistical::mean(data),
+        median: statistical::median(data),
+        population_standard_deviation:
+            statistical::population_standard_deviation(data, None),
+        population_variance: statistical::population_variance(data, None),
+        standard_deviation: statistical::standard_deviation(data, None),
+        variance: statistical::variance(data, None),
+    }
+}
+
+
 fn generate_box_plot(
     rust_data: Vec<f64>,
     cpp_data: Vec<f64>,
@@ -51,7 +106,6 @@ fn generate_box_plot(
     plot.set_layout(generate_layout());
     
     plot.show();
-    // plot.show_image(ImageFormat::SVG, 1080, 720);
     plot.write_image(output_filename, ImageFormat::PNG, 1000, 500, 4.0);
 }
 
@@ -60,10 +114,7 @@ fn generate_layout() -> Layout {
     let mut layout = Layout::new()
         .height(500)
         .width(1000)
-        .y_axis(
-            Axis::new()
-                .title(Title::new("Throughput (Mpps)")),
-        )
+        .y_axis(Axis::new().title(Title::new("Throughput (Mpps)")))
         .show_legend(false);
     
     // Add reference line
