87872af50ab7dd0a742bb5a0569ebaeabd6ae1a6
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-11-28 16:31:14 +0100

    Indent C++ with tabs

--- cpp-tests/src/perf_send.cpp
+++ cpp-tests/src/perf_send.cpp
@@ -13,15 +13,15 @@
 
 // help message for command line usage
 const std::string help_brief = "Usage:  nethuns-send [ options ]\n" \
-                                "Use --help (or -h) to see full option list and a complete description.\n\n"
-                                "Required options: \n" \
-                                "\t\t\t[ -i <ifname> ] \t set network interface \n" \
-                                "Other options: \n" \
-                                "\t\t\t[ -b <batch_sz> ] \t set batch size \n" \
-                                "\t\t\t[ -z ] \t\t\t enable send zero-copy \n" \
-                                "\t\t\t[ -n ] \t\t\t set number of packets \n" \
-                                "\t\t\t[ -s ] \t\t\t set packet size \n";
-                                
+								"Use --help (or -h) to see full option list and a complete description.\n\n"
+								"Required options: \n" \
+								"\t\t\t[ -i <ifname> ] \t set network interface \n" \
+								"Other options: \n" \
+								"\t\t\t[ -b <batch_sz> ] \t set batch size \n" \
+								"\t\t\t[ -z ] \t\t\t enable send zero-copy \n" \
+								"\t\t\t[ -n ] \t\t\t set number of packets \n" \
+								"\t\t\t[ -s ] \t\t\t set packet size \n";
+								
 
 // nethuns socket
 nethuns_socket_t* out = new nethuns_socket_t();
@@ -49,22 +49,22 @@ std::atomic<bool> term(false);
 // termination signal handler
 void terminate(int exit_signal)
 {
-    (void)exit_signal;
-    term.store(true, std::memory_order_relaxed);
+	(void)exit_signal;
+	term.store(true, std::memory_order_relaxed);
 }
 
 void terminate_program(std::chrono::system_clock::time_point stop_timestamp) {
-    std::this_thread::sleep_until(stop_timestamp);
-    term.store(true, std::memory_order_relaxed);
+	std::this_thread::sleep_until(stop_timestamp);
+	term.store(true, std::memory_order_relaxed);
 }
 
 void meter() {
-    auto now = std::chrono::system_clock::now();
-    while (!term.load(std::memory_order_relaxed)) {
-        now += std::chrono::seconds(METER_RATE_SECS);
-        std::this_thread::sleep_until(now);
-    	std::cout << total.exchange(0) << std::endl;
-    }
+	auto now = std::chrono::system_clock::now();
+	while (!term.load(std::memory_order_relaxed)) {
+		now += std::chrono::seconds(METER_RATE_SECS);
+		std::this_thread::sleep_until(now);
+		std::cout << total.exchange(0) << std::endl;
+	}
 }
 
 
@@ -74,178 +74,178 @@ void parse_command_line(int argc, char *argv[]);
 // setup and fill transmission ring
 inline void fill_tx_ring(const unsigned char *payload, int pkt_size)
 {
-    unsigned int j;
+	unsigned int j;
 
-    out = nethuns_open(&netopt, errbufs);
-    if (!out) {
-        throw std::runtime_error(errbufs);
-    }
-    
-    if (nethuns_bind(out, interface.c_str(), NETHUNS_ANY_QUEUE) < 0) {
-        throw nethuns_exception(out);
-    }
+	out = nethuns_open(&netopt, errbufs);
+	if (!out) {
+		throw std::runtime_error(errbufs);
+	}
+	
+	if (nethuns_bind(out, interface.c_str(), NETHUNS_ANY_QUEUE) < 0) {
+		throw nethuns_exception(out);
+	}
 
-    // fill the slots in the tx ring (optimized send only)
-    if (zerocopy) {
-        for (j = 0; j < nethuns_txring_get_size(out); j++) {
-            uint8_t *pkt = nethuns_get_buf_addr(out, j);    // tell me where to copy the j-th packet to be transmitted
-            memcpy(pkt, payload, pkt_size);                         // copy the packet
-        }
-        pktid = 0;                                          // first position (slot) in tx ring to be transmitted
-    }
+	// fill the slots in the tx ring (optimized send only)
+	if (zerocopy) {
+		for (j = 0; j < nethuns_txring_get_size(out); j++) {
+			uint8_t *pkt = nethuns_get_buf_addr(out, j);    // tell me where to copy the j-th packet to be transmitted
+			memcpy(pkt, payload, pkt_size);                         // copy the packet
+		}
+		pktid = 0;                                          // first position (slot) in tx ring to be transmitted
+	}
 }
 
 // transmit packets in the tx ring (use optimized send, zero copy)
 inline void transmit_zc(int pkt_size)
 {
-    // prepare batch
-    for (int n = 0; n < batch_size; n++) {
-        if (nethuns_send_slot(out, pktid, pkt_size) <= 0) {
-            break;
-        }
-        pktid++;
-        total++;
-    }
-    nethuns_flush(out);             // send batch
+	// prepare batch
+	for (int n = 0; n < batch_size; n++) {
+		if (nethuns_send_slot(out, pktid, pkt_size) <= 0) {
+			break;
+		}
+		pktid++;
+		total++;
+	}
+	nethuns_flush(out);             // send batch
 }
 
 // transmit packets in the tx ring (use classic send, copy)
 inline void transmit_c(const unsigned char *payload, int pkt_size)
 {
-    // prepare batch
-    for (int n = 0; n < batch_size; n++) {
-        if (nethuns_send(out, payload, pkt_size) <= 0) {
-            break;
-        }
-        total++;
-    }
-    nethuns_flush(out);             // send batch
+	// prepare batch
+	for (int n = 0; n < batch_size; n++) {
+		if (nethuns_send(out, payload, pkt_size) <= 0) {
+			break;
+		}
+		total++;
+	}
+	nethuns_flush(out);             // send batch
 }
 
 inline std::chrono::system_clock::time_point next_meter_log() {
-    return std::chrono::system_clock::now() + std::chrono::seconds(METER_RATE_SECS);
+	return std::chrono::system_clock::now() + std::chrono::seconds(METER_RATE_SECS);
 }
 
 
 int main(int argc, char *argv[])
 {
-    static const unsigned char payload[PAYLOAD_LEN] =
-    {
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf0, 0xbf, /* L`..UF.. */
-        0x97, 0xe2, 0xff, 0xae, 0x08, 0x00, 0x45, 0x00, /* ......E. */
-        0x00, 0x54, 0xb3, 0xf9, 0x40, 0x00, 0x40, 0x11, /* .T..@.@. */
-        0xf5, 0x32, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, /* .2...... */
-        0x07, 0x08
-    };
-    
-    parse_command_line(argc, argv);
-    
-    signal(SIGINT, terminate);  // register termination signal
-    
-    // nethuns options
-    netopt = {
-        .numblocks       = 1
-    ,   .numpackets      = numpackets
-    ,   .packetsize      = packetsize
-    ,   .timeout_ms      = 0
-    ,   .dir             = nethuns_in_out
-    ,   .capture         = nethuns_cap_zero_copy
-    ,   .mode            = nethuns_socket_rx_tx
-    ,   .promisc         = false
-    ,   .rxhash          = false
-    ,   .tx_qdisc_bypass = true
-    ,   .xdp_prog        = nullptr
-    ,   .xdp_prog_sec    = nullptr
-    ,   .xsk_map_name    = nullptr
-    ,   .reuse_maps      = false
-    ,   .pin_dir         = nullptr
-    };
-    
-    // Init nethuns socket
-    fill_tx_ring(payload, PAYLOAD_LEN);
-    
-    // set up timer for stopping data collection after 10 minutes
-    std::thread stop_th(
-        terminate_program, 
-        std::chrono::system_clock::now() + std::chrono::seconds(METER_DURATION_SECS)
-    );
-    
-    std::thread meter_th(meter);
-    
-    try {
-        while (!term.load(std::memory_order_relaxed)) {            
-            if (zerocopy) {
-                transmit_zc(PAYLOAD_LEN);
-            }
-            else {
-                transmit_c(payload, PAYLOAD_LEN);
-            }
-        }
-    } catch(nethuns_exception &e) {
-        if (e.sock) {
-            nethuns_close(e.sock);
-        }
-        std::cerr << e.what() << std::endl;
-        return 1;
-    } catch(std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
-    
-    nethuns_close(out);
-    return 0;
+	static const unsigned char payload[PAYLOAD_LEN] =
+	{
+		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf0, 0xbf, /* L`..UF.. */
+		0x97, 0xe2, 0xff, 0xae, 0x08, 0x00, 0x45, 0x00, /* ......E. */
+		0x00, 0x54, 0xb3, 0xf9, 0x40, 0x00, 0x40, 0x11, /* .T..@.@. */
+		0xf5, 0x32, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, /* .2...... */
+		0x07, 0x08
+	};
+	
+	parse_command_line(argc, argv);
+	
+	signal(SIGINT, terminate);  // register termination signal
+	
+	// nethuns options
+	netopt = {
+		.numblocks       = 1
+	,   .numpackets      = numpackets
+	,   .packetsize      = packetsize
+	,   .timeout_ms      = 0
+	,   .dir             = nethuns_in_out
+	,   .capture         = nethuns_cap_zero_copy
+	,   .mode            = nethuns_socket_rx_tx
+	,   .promisc         = false
+	,   .rxhash          = false
+	,   .tx_qdisc_bypass = true
+	,   .xdp_prog        = nullptr
+	,   .xdp_prog_sec    = nullptr
+	,   .xsk_map_name    = nullptr
+	,   .reuse_maps      = false
+	,   .pin_dir         = nullptr
+	};
+	
+	// Init nethuns socket
+	fill_tx_ring(payload, PAYLOAD_LEN);
+	
+	// set up timer for stopping data collection after 10 minutes
+	std::thread stop_th(
+		terminate_program, 
+		std::chrono::system_clock::now() + std::chrono::seconds(METER_DURATION_SECS)
+	);
+	
+	std::thread meter_th(meter);
+	
+	try {
+		while (!term.load(std::memory_order_relaxed)) {            
+			if (zerocopy) {
+				transmit_zc(PAYLOAD_LEN);
+			}
+			else {
+				transmit_c(payload, PAYLOAD_LEN);
+			}
+		}
+	} catch(nethuns_exception &e) {
+		if (e.sock) {
+			nethuns_close(e.sock);
+		}
+		std::cerr << e.what() << std::endl;
+		return 1;
+	} catch(std::exception &e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	
+	nethuns_close(out);
+	return 0;
 }
 
 
 void parse_command_line(int argc, char *argv[])
 {
-    // parse options from command line
-    int opt = 0;
-    int optidx = 0;
-    opterr = 1;     // turn on/off getopt error messages
-    if (argc > 1 && argc < 10) {
-        while ((opt = getopt_long(argc, argv, "hi:b:mzn:s:", NULL, &optidx)) != -1) {
-            switch (opt) {
-            case 'h':
-                std::cout << help_brief << std::endl;
-                std::exit(0);
-                return;
-            case 'i':
-                if (optarg)
-                    interface = optarg;
-                break;
-            case 'b':
-                if (optarg)
-                    batch_size = atoi(optarg);
-                break;
-            case 'z':
-                zerocopy = true;
-                break;
-            case 'n':
-                if (optarg)
-                    numpackets = atoi(optarg);
-                break;
-            case 's':
-                if (optarg)
-                    packetsize = atoi(optarg);
-                break;
-            default:
-                std::cerr << "Error in parsing command line options.\n" << help_brief << std::endl;
-                std::exit(-1);
-                return;
-            }
-        }
-    } else {
-        std::cerr << help_brief << std::endl;
-        std::exit(-1);
-        return;
-    }
+	// parse options from command line
+	int opt = 0;
+	int optidx = 0;
+	opterr = 1;     // turn on/off getopt error messages
+	if (argc > 1 && argc < 10) {
+		while ((opt = getopt_long(argc, argv, "hi:b:mzn:s:", NULL, &optidx)) != -1) {
+			switch (opt) {
+			case 'h':
+				std::cout << help_brief << std::endl;
+				std::exit(0);
+				return;
+			case 'i':
+				if (optarg)
+					interface = optarg;
+				break;
+			case 'b':
+				if (optarg)
+					batch_size = atoi(optarg);
+				break;
+			case 'z':
+				zerocopy = true;
+				break;
+			case 'n':
+				if (optarg)
+					numpackets = atoi(optarg);
+				break;
+			case 's':
+				if (optarg)
+					packetsize = atoi(optarg);
+				break;
+			default:
+				std::cerr << "Error in parsing command line options.\n" << help_brief << std::endl;
+				std::exit(-1);
+				return;
+			}
+		}
+	} else {
+		std::cerr << help_brief << std::endl;
+		std::exit(-1);
+		return;
+	}
 
-    std::cout << "\nTest " << argv[0] << " started with parameters \n"
-              << "* interface: " << interface << " \n"
-              << "* batch_size: " << batch_size << " \n"
-              << "* zero-copy: " << ((zerocopy) ? " ON \n" : " OFF \n")
-              << "* numpackets: " << numpackets << " \n"
-              << "* packetsize: " << packetsize << " \n"
-              << std::endl;
+	std::cout << "\nTest " << argv[0] << " started with parameters \n"
+			  << "* interface: " << interface << " \n"
+			  << "* batch_size: " << batch_size << " \n"
+			  << "* zero-copy: " << ((zerocopy) ? " ON \n" : " OFF \n")
+			  << "* numpackets: " << numpackets << " \n"
+			  << "* packetsize: " << packetsize << " \n"
+			  << std::endl;
 }
