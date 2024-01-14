63cda1d16140e4eed00f45118c2632e669ac8164
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2024-01-14 17:22:27 +0100

    Improve performance graphs

--- recv_analysis.sh
+++ recv_analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd performance-analysis
-RUST_BACKTRACE=1 cargo run -- ../data/recv_rust.txt ../data/recv_cpp.txt ../data/recv "Recv" | tee ../data/recv.stats
+RUST_BACKTRACE=1 cargo run -- ../data/recv_rust.txt ../data/recv_cpp.txt ../data/recv "Reception" | tee ../data/recv.stats
 cd -
